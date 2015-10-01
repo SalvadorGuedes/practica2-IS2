1c0c6ae67eb98ccf84affc5c85579b0591a43773
Salvador <Salvador@10.230.169.39>
2015-10-01 09:26:49 +0100

    Primer commit 1.Oct.2015

---
+++ src/practica2/Main.java
@@ -0,0 +1,18 @@
+
+package practica2;
+
+import java.util.HashMap;
+
+public class Main {
+
+    public static void main(String[] args) {
+        int[] vector = {1,1,1,3,4,5,6,9,100,100,-4,-4};
+        Histogram histo = new Histogram(vector);
+        HashMap <Integer,Integer> histogram = histo.getHisto();
+        
+        for (int key : histogram.keySet()) {
+             System.out.println(key+ " : "+ histogram.get(key));
+        }
+    }
+    
+}
